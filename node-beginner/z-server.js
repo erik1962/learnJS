6916292c4a09d9038b25aaa1bc69dbbb6d575b1f
Erik Vander Ahe <evanderahe@gmail.com>
2021-02-25 11:04:04 -0500

    Non-blocking request handlers implemented

--- node-beginner/z-server.js
+++ node-beginner/z-server.js
@@ -9,10 +9,7 @@ function start(route, handle) {
         let pathname = url.parse(request.url).pathname;
         console.log(`request.url: ${request.url} received.`);
         console.log(`parsed pathname: ${pathname} received.`);
-        let content = route(handle, pathname);
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(content);
-        response.end();
+        let content = route(handle, pathname, response);
     }
         
     http.createServer(onRequest).listen(8888);
